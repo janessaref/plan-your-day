e8ba4c202f48c4bb85f189e9249f2b6362fc4c65
Game Over <janessarefong@gmail.com>
2020-09-26 07:34:03 -0700

    edit value in html for textarea and add color to text boxes in js

--- script.js
+++ script.js
@@ -7,28 +7,54 @@ Guide for day planner construction:
 */ 
 
 // Today's date from moment.js
-// var saveToDo = [];
+var saveToDo = [];
+
 moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").append(moment().format('dddd, MMMM Do'));  
 
-// $(".saveBtn").on("click",function() {
-//     console.log("hello");
+$(".saveBtn").on("click",function() {
+    console.log("hello");
    
-//      var save = $(this).attr("data-time");
-//     var saveData = $(save).val();
+    var save = $(this).attr("data-time");
+    var saveData = $(save).val();
+    
 
-//     saveToDo.push(saveData);
+    saveToDo.push(save);
 
-//     localStorage.setItem(save, saveToDo);
+    localStorage.setItem("saveToDo", JSON.stringify(saveToDo));
 
-// });
+});
 
 // colors of time of day
 var timeofDay = moment().format('MMMM Do YYYY, h:mm:ss a');
 
 var hourofDay = moment().format('H');  
 
+var timeArray = [];
 
 
+// var timeVal = $(".hour").attr("data-value");
+// timeArray.push(timeVal);
+// console.log(timeArray)
+
+var textBox = $("textarea");
+console.log(textBox)
+
+
+for(var i = 0; i < textBox.length; i++) {
+    var everyHr = (textBox[i]).getAttribute("value");
+    console.log(everyHr)
+
+    
+    if (everyHr === hourofDay) {
+        textBox.addClass("present");
+    }
+    else if (everyHr > hourofDay) {
+        textBox.addClass("future");
+    }
+    else {
+        textBox.addClass("past");
+    }
+}
 
 
